f4aa4152ba032dba7b39c7029fcac95c01c7d45f
Benja-Dev <begalan@outlook.es>
2024-12-21 10:20:46 -0600

    Acrtualizado

--- config/database.php
+++ config/database.php
@@ -84,18 +84,19 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL'), // Usa solo esta variable para la URL completa
+            'host' => env('DB_HOST', '127.0.0.1'), // Este valor debería ser ignorado si usas `DATABASE_URL`
+            'port' => env('DB_PORT', '5432'), // También ignorado si usas `DATABASE_URL`
+            'database' => env('DB_DATABASE', 'forge'), // Igual
+            'username' => env('DB_USERNAME', 'forge'), // Igual
+            'password' => env('DB_PASSWORD', ''), // Igual
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require', // Importante para la conexión segura con Supabase
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
@@ -147,7 +148,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
